246c54bcaa37ea3f32200dda5b18584fb856f61e
Adam <adam@adam.com>
2022-12-12 08:13:57 -0300

    feat: Second day with two stars.

--- src/day_02.sh
+++ src/day_02.sh
@@ -19,14 +19,31 @@ declare -A resultado=(
 	["C X"]="7" 
 	["C Y"]="2" 
 	["C Z"]="6")
+
+declare -A result_part2=( 
+	["A X"]="3" 
+	["A Y"]="4" 
+	["A Z"]="8" 
+	["B X"]="1" 
+	["B Y"]="5" 
+	["B Z"]="9" 
+	["C X"]="2" 
+	["C Y"]="6" 
+	["C Z"]="7")
+
 #----------------------Codigo-------------------------------------------------#
 while read linha 
-
 do
-	pontuacao=$(( $resultado + ${resultado["$linha"]} ))
+	if test -n "$linha"
+	then
+		pontuacao=$(( $pontuacao + ${resultado["$linha"]} ))
+		second_score=$(( $second_score + ${result_part2["$linha"]} ))
+	fi
+
 done < "$1"
 
-echo $pontuacao
+echo "first star $pontuacao"
+echo "second star $second_score"
 
 #-----------------------------------------------------------------------------#
 
