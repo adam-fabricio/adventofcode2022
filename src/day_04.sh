abb0f4caea25ee79dba9f8ebe1fd87c618895651
adam <adam@adam>
2022-12-15 09:00:01 -0300

    feat: add Skeleton of days

--- src/day_04.sh
+++ src/day_04.sh
@@ -8,21 +8,6 @@ test -z "$1" && echo "Erro!"  && exit 1
 sum_fully_contains=0
 sum_intersections=0
 #-----------------------------------Functions---------------------------------#
-range_to_section() {
-	IFS="-" read -a range <<< "$1"
-	echo $(seq ${range[@]})
-}
-
-fully_contains() {
-	section1=$(range_to_section "$1")
-	section2=$(range_to_section "$2")
-	if [ "${#section1}" -le "${#section2}" ] 
-	then
-		[[ " "$section2" " == *" "$section1" "* ]] && echo "1" || echo "0"
-	else
-		[[ " "$section1" " == *""$section2" "* ]] && echo "1" || echo "0"
-	fi
-}
 #------------------------------------Codes------------------------------------#
 while read line
 do
