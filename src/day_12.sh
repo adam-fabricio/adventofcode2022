97aecc078384b2c4cdeb84f7724d0d8b10897926
Adam <adam@adam.com>
2023-01-05 05:42:04 -0300

    feat: day_12: continue

--- src/day_12.sh
+++ src/day_12.sh
@@ -6,10 +6,17 @@
 test -z "$1" && echo "Erro!"  && exit 1 
 #-----------------------------------Variable----------------------------------#
 declare -A map
-declare -i col=0
-declare -i lin=0
+declare -i col=1
+declare -i lin=1
 declare -- start=""
 declare -- finish=""
+declare -a visited
+declare -a queue
+declare -i dist
+declare -a neighbor_lin
+declare -a neighbor_col
+declare -a neighbor
+read -r max_lin max_col <<< $(wc -lL < $1)
 #-----------------------------------Functions---------------------------------#
 #-----------------------------------MAP Parser--------------------------------#
 while read -N 1 char
@@ -22,13 +29,27 @@ do
         col=col+1 
     else
         lin=lin+1
-        col=0 
+        col=1 
         continue
     fi
 done < $1
 #--------------------------------------|--------------------------------------#
 declare -p map start finish
 #---------------------------------First Star----------------------------------#
+visited+=( "$start" )
+queue+=( "$start" )
+
+while :
+do
+	read -r lin col <<< $(echo "${queue[0]}")
+	[[ "$lin" -gt 1 ]] && { let lin--; neighbor_lin+=( "$lin" ); } 
+	[[ "$lin" -lt "$max_lin" ]] && { let lin++; neighbor_lin+=( "$lin" ); }
+	[[ "$col" -gt 1 ]] && { let col--; neighbor_col+=( "$col" ); } 
+	[[ "$col" -lt "$max_col" ]] && { let col++; neighbor_col+=( "$col" ); }
+	declare -p neighbor_lin neighbor_col
+	break
+
+done
 
 #---------------------------------Second Star---------------------------------#
 #-------------------------------------END-------------------------------------#
