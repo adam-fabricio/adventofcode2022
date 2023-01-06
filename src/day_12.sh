0e42db5b41fd62eae741aba4439db117095231e8
Adam <adam@adam.com>
2023-01-06 05:42:33 -0300

    feat: day_12: first star finish

--- src/day_12.sh
+++ src/day_12.sh
@@ -35,12 +35,62 @@ do
     fi
 done < $1
 #--------------------------------------|--------------------------------------#
-#---------------------------------First Star----------------------------------#
-map[$start]="98 0"
-map[$finish]="122"
 
-visited+=( "$start" )
-queue+=( "$start" )
+##---------------------------------First Star----------------------------------#
+#
+#map[$start]="98 0"
+#map[$finish]="122"
+#
+#visited+=( "$start" )
+#queue+=( "$start" )
+#
+#
+#while :
+#do
+#	read -r col lin <<< "${queue[0]}"
+#    #----------------------------Create neighbor---------------------------#
+#    [[ "$lin" -gt 1 ]] && neighbors+=( "$col $(($lin-1))" ) 
+#    [[ "$lin" -lt "$max_lin" ]] && neighbors+=( "$col $(($lin+1))" )
+#    [[ "$col" -gt 1 ]] && neighbors+=( "$(($col-1)) $lin" ) 
+#    [[ "$col" -lt "$max_col" ]] && neighbors+=( "$(($col+1)) $lin" )
+#    #---------------------------check neighbor-----------------------------#
+#    for neighbor in "${neighbors[@]}"
+#    do
+#        #-----check if the neighbor has already been visited----#
+#        for each_visited in "${visited[@]}"
+#        do
+#            [[ "$each_visited" == "$neighbor" ]] && continue 2
+#        done
+#        #-----------Get value for map---------------------------#
+#        read -r map_value map_dist <<< ${map[$col $lin]}
+#        #--------------------Check elevation--------------------#
+#        #elevation=${map_value}-${map[$neighbor]}
+#        elevation=${map[$neighbor]}-$map_value
+#        [[ ${elevation} -gt 1 ]] && continue
+#        #------------------Add to queue-------------------------#
+#        queue+=( "$neighbor" )
+#        #----------------mark as visited array------------------#
+#        visited+=( "$neighbor" )
+#        #----------------Add distance to map--------------------#
+#        map["$neighbor"]+=" $(($map_dist+1))"
+#        #------Check if best signal position--------------------#
+#        [[ "$neighbor" == "$finish" ]] && break 2
+#    done
+#    
+#    #----------------------unset neighbors-----------------------------------#
+#	neighbors=( )
+#    queue=( "${queue[@]:1}" )
+#done
+##exit
+#echo "${map[$finish]}"
+#
+#
+#---------------------------------Second Star---------------------------------#
+map[$start]="97"
+map[$finish]="122 0"
+
+visited+=( "$finish" )
+queue+=( "$finish" )
 
 
 while :
@@ -62,8 +112,8 @@ do
         #-----------Get value for map---------------------------#
         read -r map_value map_dist <<< ${map[$col $lin]}
         #--------------------Check elevation--------------------#
-        elevation=${map_value}-${map[$neighbor]}
-        [[ ${elevation#-} -gt 1 ]] && continue
+		elevation=${map[$neighbor]}-$map_value
+        [[ ${elevation} -lt -1 ]] && continue
         #------------------Add to queue-------------------------#
         queue+=( "$neighbor" )
         #----------------mark as visited array------------------#
@@ -71,25 +121,26 @@ do
         #----------------Add distance to map--------------------#
         map["$neighbor"]+=" $(($map_dist+1))"
         #------Check if best signal position--------------------#
-        [[ "$neighbor" == "$finish" ]] && break 2
+		[[ $map_value -eq "97" ]] && break 2
     done
-    
-    #----------------------unset neighbors-----------------------------------#
-    neighbors=( )
+    declare -p queue
+	#----------------------unset neighbors-----------------------------------#
+	neighbors=( )
     queue=( "${queue[@]:1}" )
 done
-
-for ((i=1 ; i<=$max_lin ; i++))
+declare -p map
+for ((i=1 ; i<=$max_lin ; i++ ))
 do
-    for ((j=1 ; j<=$max_col ; j++))
-    do
-        read -r val dist <<< ${map[$j $i]}
-        echo -ne "$dist\t"
-    done
-    echo
+	echo -n "$i->"
+	for ((j=1 ; j<=$max_col; j++ ))
+	do
+		read -r loc val <<< ${map[$j $i]}
+		echo -ne "$loc|$val\t"
+	done
+	echo -n "<-$i"
+	echo
 done
-echo "${map[$finish]}"
-#---------------------------------Second Star---------------------------------#
+
 #-------------------------------------END-------------------------------------#
 #-----------------------------------Results-----------------------------------#
 #--------------------------------------|--------------------------------------#
