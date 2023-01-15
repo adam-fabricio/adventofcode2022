90d561f13a5a6e2e6484971e30d8a8f043928a9e
adam <adam>
2023-01-15 17:19:37 -0300

    feat: day_14: Finish parsin

--- src/day_14.sh
+++ src/day_14.sh
@@ -17,33 +17,66 @@ else
 fi
 #-------------------------------------Vars------------------------------------#
 declare -A solid
+declare -i line_number=1
+declare -i abyss=0
+declare -i floor
+declare -- sand
+declare -- sand_fall
+declare -i sand_x=500
+declare -i sand_y
 #----------------------------------Read data input----------------------------#
-
-line_number=1
 while read list_coordinates
 do
 	IFS=' -> ' read -a coordinates <<< "$list_coordinates"
 	coordinates=( ${coordinates[@]} )
 	for (( i=1 ; i<${#coordinates[@]} ; i++ ))
-	do
+	do	
+		#  Split ordered pair
 		x0=$(cut -d',' -f1 <<< ${coordinates[i-1]})
 		y0=$(cut -d',' -f2 <<< ${coordinates[i-1]})
 		x1=$(cut -d',' -f1 <<< ${coordinates[i]})
 		y1=$(cut -d',' -f2 <<< ${coordinates[i]})
-		for line in $(eval echo {$x0..$x1})
+		
+		#  Find the abyss 	
+		[[ $y0 -gt $abyss ]] && abyss=$y0
+		[[ $y1 -gt $abyss ]] && abyss=$y1
+	    
+		for x in $(eval echo {$x0..$x1})
 		do
-			for col in $(eval echo {$y0..$y1})
+			for y in $(eval echo {$y0..$y1})
 			do
-				if [[ -z "${solid[$line,$col]}" ]]
+				if [[ -z "${solid[$x,$y]}" ]]
 				then
-					solid["$line,$col"]=1
+					solid["$x,$y"]=1
+				fi
+				#  Find the first floor in col 500
+				if [[ $x -eq 500 ]]
+				then
+					[[ -z $floor ]] && floor=$y
+					[[ $y -lt $floor ]] && floor=$y
 				fi
 			done
 		done
 	done
-    let line_number++
 done < "$data_input" 
+echo "Number of rocks: ${#solid[@]}"
+#echo "Rocks ${!solid[@]}"
+sand_y=$floor
+set -x
+sand_fall="$sand_x,$sand_y"
+echo "first sand: $sand"
+declare -p sand
+set +x
+echo "Abyss in line: $abyss"
 
-echo "${#solid[@]}"
+while [[ $sand_x -le $abyss ]]; then
+	while :
+		
+		[[ $sand_x,$sand_y+1
+	
+	
+	
+
+
 #--------------------------------------|--------------------------------------#
 #--------------------------------------|--------------------------------------#
