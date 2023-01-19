c165361929ea680defdf3b8387bd124c8651e235
adam <adam@adam>
2023-01-18 21:19:43 -0300

    feat: day_15: first star

--- src/day_15.sh
+++ src/day_15.sh
@@ -5,8 +5,12 @@
 #---------------------------------Start Date----------------------------------#
 #
 #    1674068124 -> 18/01/23 15:55:24
+#    1674087435 -> 18/01/23 21:17:15 -> First star
 #
+#------------------------------------Vas-------------------------------------#
+declare -A row_10
 #----------------------------------Data input---------------------------------#
+
 if [[ "$1" == test ]]
 then
     data_input="data/day_15_test"
@@ -27,12 +31,32 @@ do
 	beacon_y=$(sed -r 's/.*=//' <<< $line)
 	distance_x=$(( $sensor_x - $beacon_x ))
 	distance_y=$(( $sensor_y - $beacon_y ))
-	distance=$(( ${distance_x#-} - ${distance_y#-} ))
+	distance_to_beacon=$(( ${distance_x#-} + ${distance_y#-} ))
+    distance_to_row=$(( $sensor_y - $row ))
+    signal=$(( ${distance_to_beacon#-} - ${distance_to_row#-} ))
 
-	echo
-	echo "Sensor: ($sensor_x, $sensor_y) beacon: ($beacon_x, $beacon_y)
-	distance: $distance"
-	echo
+    if [[ $signal -ge 0 ]]; then
+        i=$(($sensor_x-${signal#-}))
+        while [ $i -le $(($sensor_x+${signal#-})) ]
+        do
+            row_10[$i]=1
+            let i++
+        done
+    fi
+
+#	echo
+#	echo "\
+#Sensor: ($sensor_x, $sensor_y) \
+#beacon: ($beacon_x, $beacon_y) \
+#distance to beacon : $distance_to_beacon \
+#distance to row: $distance_to_row \
+#signal: $signal \
+#row: ${#row_10[@]}
+#"
+#	echo
 done < "$data_input" 
+
+echo "row= $(( ${#row_10[@]} -1 ))"
+
 #--------------------------------------|--------------------------------------#
 #-----------------------------------Test input--------------------------------#
