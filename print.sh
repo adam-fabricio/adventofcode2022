57c2b2b4edfffe1a84d077d838a3944abb5d8dfd
adam <adam>
2023-01-24 17:24:10 -0300

    feat: day_15: try other way

---
+++ print.sh
@@ -0,0 +1,32 @@
+
+for ((lin=0; lin<=30; lin++ )); do
+	echo -ne "$lin ->\t"
+	for ((col=0; col<=30 ; col++)); do
+		x_0=$1
+		y_0=$2
+		r_0=$3
+		delta_x_0=$(( x_0 - col ))
+		delta_x_0=${delta_x_0#-}
+
+		x_1=$4
+		y_1=$5
+		r_1=$6
+		delta_x_1=$(( x_1 - col ))
+		delta_x_1=${delta_x_1#-}
+		
+		if [[ col -eq 14 ]] && [[ lin -eq 11 ]]; then
+			echo -n 'X'
+		elif [[ delta_x_0 -le r_0 ]] && [[ lin -eq $((y_0-r_0+delta_x_0)) ]]; then
+			echo -n 'A'
+		elif [[ delta_x_0 -le r_0 ]] && [[ lin -eq $((y_0+r_0-delta_x_0)) ]]; then
+			echo -n 'A'
+		elif [[ delta_x_1 -le r_1 ]] && [[ lin -eq $((y_1-r_1+delta_x_1)) ]]; then
+			echo -n 'B'
+		elif [[ delta_x_1 -le r_1 ]] && [[ lin -eq $((y_1+r_1-delta_x_1)) ]]; then
+			echo -n 'B'
+		else
+			echo -n '.'
+		fi
+	done
+	echo
+done
--- src/day_15.sh
+++ src/day_15.sh
@@ -8,7 +8,7 @@
 #    1674087435 -> 18/01/23 21:17:15 -> First star
 #
 #------------------------------------Vas-------------------------------------#
-declare -A candidate_beacon
+
 
 #----------------------------------Data input---------------------------------#
 
@@ -36,6 +36,10 @@ do
     distance_to_row=$(( $sensor_y - $row ))
     signal=$(( ${distance_to_beacon#-} - ${distance_to_row#-} ))
 
+	sensors_x+=( $sensor_x )
+	sensors_y+=( $sensor_y )
+	radius+=( $distance_to_beacon )
+
 ## First Star
 
 #    if [[ $signal -ge 0 ]]; then
@@ -46,28 +50,59 @@ do
 #            let i++
 #        done
 #    fi
-
-	echo "sensor -> ($sensor_x,$sensor_y) Raio = $((distance_to_beacon+1))"
-	x_min=$((sensor_x-distance_to_beacon-1))
-	[[ x_min -lt 0 ]] && x_min=0
-	x_max=$((sensor_x+distance_to_beacon+1))
-	[[ x_max -gt 4000000 ]] && x_max=4000000
-	for (( x=x_min ; x<=x_max ; x++ ))
-	do
-		delta_x=$((sensor_x-x))
-		y=$((sensor_y+distance_to_beacon+1-${delta_x#-}))
-		candidate_beacon["$x,$y"]=1
-		y=$((sensor_y-distance_to_beacon-1+${delta_x#-}))
-		candidate_beacon["$x,$y"]=1
-
-	done
-	echo -n "${#candidate_beacon[@]} -> "
-	echo ${!candidate_beacon[@]}
-	unset candidate_beacon
-	declare -A candidate_beacon
-
+#
+#	echo "sensor -> ($sensor_x,$sensor_y) Raio = $((distance_to_beacon+1))"
+#	x_min=$((sensor_x-distance_to_beacon-1))
+#	[[ x_min -lt 0 ]] && x_min=0
+#	x_max=$((sensor_x+distance_to_beacon+1))
+#	[[ x_max -gt 4000000 ]] && x_max=4000000
+#	for (( x=x_min ; x<=x_max ; x++ ))
+#	do
+#		delta_x=$((sensor_x-x))
+#		y=$((sensor_y+distance_to_beacon+1-${delta_x#-}))
+#		candidate_beacon["$x,$y"]=1
+#		y=$((sensor_y-distance_to_beacon-1+${delta_x#-}))
+#		candidate_beacon["$x,$y"]=1
+#
+#	done
+#	echo -n "${#candidate_beacon[@]} -> "
+#	echo ${!candidate_beacon[@]}
+#	unset candidate_beacon
+#	declare -A candidate_beacon
+#
 
 done < "$data_input" 
+
+num_itens=${#sensors_x[@]}
+
+for (( i=0; i<num_itens-1; i++ )); do
+	for (( j=i+1 ;j<num_itens; j++ )); do
+		
+		distance_x=$(( ${sensors_x[i]} - ${sensors_x[j]} ))
+		distance_y=$(( ${sensors_y[i]} - ${sensors_y[j]} ))
+		distance=$(( ${distance_x#-} + ${distance_y#-} ))
+	
+		radius_sum=$(( ${radius[i]} + ${radius[j]} ))
+
+		signal_shadow=$(( distance - radius_sum ))
+
+		[[ $signal_shadow -ne 2 ]] && continue
+		
+		echo -n "sensor_$i->(${sensors_x[$i]},${sensors_y[$i]}) "
+		echo -n "radious->${radius[$i]} || "
+	    echo -n "sensor_$j->(${sensors_x[$j]},${sensors_y[$j]}) "
+		echo "radius->${radius[$j]}"
+
+		echo -n "distance between sensors=$distance || "
+		echo "sum of radius=$radius_sum"
+		#echo "Diference raius sensors=$signal_shadow"
+
+		echo
+		echo "###"
+		echo
+
+	done
+done	
 
 #echo "row= $(( ${#row_array[@]} -1 ))"
 
