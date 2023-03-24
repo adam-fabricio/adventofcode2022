d196663cceaec737ddf9419b05799675cb3a0628
adam <a@a>
2023-03-24 14:00:37 -0300

    feat: day_19: continue

--- src/day_19.sh
+++ src/day_19.sh
@@ -21,18 +21,17 @@ function calc_resource () {
 	local robots=( $2 $3 $4 $5 )
 	local resources=( $6 $7 $8 $9 )
 	local purchase_list=( "none" )
+	local time_elapsed=0
 
-	echo "$time -> ( ${robots[@]} ) ( ${resources[@]} )"
-	#  Pomtos de parada
-
-	[[ time -gt 24 ]] && return
-	if [[ robots[3] -eq 0 ]] && [[ robots[2] -eq 0 ]]; then
-
-
-
-
+	#  Pontos de parada
 	let time++
-	
+	time_left=$(( time_limit - time ))
+	echo "$time->robots( ${robots[@]} )->resources( ${resources[@]} )"
+	[[ robots[2] -le 1 ]] && [[ time -gt 12 ]] && return
+	[[ time -gt obsidian_robot[2] ]] && [[ robots[1] -eq 0 ]] && return
+
+	[[ time -gt geode_robot[3] ]] && [[ robots[2] -eq 0 ]] && return
+
 	[[ resources[0] -ge ore_robot[0] ]] && purchase_list+=( "ore_robot" )
 	[[ resources[0] -ge clay_robot[0] ]] && purchase_list+=( "clay_robot" )
 	[[ resources[0] -ge obsidian_robot[0] ]] &&\
@@ -48,6 +47,7 @@ function calc_resource () {
 	
 	[[ crack_geode -lt resources[3] ]] && crack_geode=${resource[3]}
 
+	[[ time_left -le 0 ]] && return
 	for purchase in ${purchase_list[@]}; do
 		case "$purchase" in
 			none)
@@ -90,16 +90,25 @@ function calc_resource () {
 
 id=1
 while read -a line; do
+	time_limit=24
 	crack_geode=0
 	ore_robot=( "${line[6]}" )
 	clay_robot=( "${line[12]}" )
-	obsidian_robot=( "${line[18]}" "${line[21]}" )
-	geode_robot=( "${line[27]}" "0" "${line[30]}" )
+	obsidian_robot=( "${line[18]}" "${line[21]}" "1" )
+	geode_robot=( "${line[27]}" "0" "${line[30]}" "1" )
+	for (( i=0; i < geode_robot[2]; geode_robot[3]++)); do
+		i=$(( geode_robot[3] + i ))
+	done
+	geode_robot[3]=$(( time_limit - geode_robot[3] + 1 ))
+	for (( i=0; i < obsidian_robot[1]; obsidian_robot[2]++ )); do
+		i=$(( obsidian_robot[2] + i ))
+	done
+	obsidian_robot[2]=$(( geode_robot[3] - obsidian_robot[2] + 1 ))
+
 	echo "$id -> ore_robot[ ${ore_robot[@]} ] clay_robot[ ${clay_robot[@]} ]\
  obsidian_robot[ ${obsidian_robot[@]} ] geode_robot[ ${geode_robot[@]} ]"
-	
 	calc_resource 0 1 0 0 0 0 0 0 0
-	
+	echo $crack_geode
 
 	let id++
 	exit
